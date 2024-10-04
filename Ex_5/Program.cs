2575e6308639f84ab02d274f527d3f010cb5cd47
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:36:49 +0300

    Add project files.

---
+++ Ex_5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть три числа:");
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        int max = Math.Max(a, Math.Max(b, c));
+        Console.WriteLine($"Найбільше число: {max}");
+    }
+}
